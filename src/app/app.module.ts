1f69a3b97917890a26838e00e8900e70073c635e
Juber Ahmed <a.juber@hotmail.com>
2018-11-04 19:13:02 -0500

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ProductRepository } from './model/product.repository';
+import { StaticDataSource } from './model/static.datasource';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from './store/store.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule
   ],
-  providers: [],
+  providers: [ProductRepository, StaticDataSource],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
