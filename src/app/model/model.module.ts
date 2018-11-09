91430058b2b6d70b25e53dc8a71e9c7cba5d8706
Juber Ahmed <a.juber@hotmail.com>
2018-11-08 20:48:52 -0500

    Added model for user checkout feature

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ProductRepository } from './product.repository';
 import { StaticDataSource } from './static.datasource';
+import { Order } from './order.model';
+import { OrderRepository } from './order.repository';
 import { Cart } from './cart.model';
 
 @NgModule({
-  providers: [ProductRepository, StaticDataSource, Cart]
+  providers: [ProductRepository, StaticDataSource, Cart, Order, OrderRepository]
 })
 export class ModelModule { }
